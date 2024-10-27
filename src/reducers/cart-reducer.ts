abf44dde4506f0792e84829628aad8378ed940e3
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-10-27 16:07:27 -0500

    add

---
+++ src/reducers/cart-reducer.ts
@@ -0,0 +1,34 @@
+import { Guitar } from "../types"
+
+
+export type CarActions =
+    {
+        type: 'add-to-cart',
+        payload: {
+            item: Guitar
+        }
+    } |
+    {
+        type: 'remove-from-cart',
+        payload: {
+            id: Guitar['id']
+        }
+    } |
+    {
+        type: 'decrease-quantity',
+        payload: {
+            id: Guitar['id']
+        }
+    } |
+    {
+        type: 'increase-quantity',
+        payload: {
+            id: Guitar['id']
+        }
+    } |
+    {
+        type: 'increase-quantity',
+        payload: {
+            id: Guitar['id']
+        }
+    }
